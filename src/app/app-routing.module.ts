3aec2604a7d95664d27fd2cfa1934266c69c30b8
trsr99 <trsr99@gmail.com>
2020-07-27 17:59:25 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -6,6 +6,7 @@ import { BasicsComponent } from './basics/basics.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { AppComponent } from './app.component';
 import { FormsComponent} from './forms/forms.component';
+import { ReactiveformsComponent} from './reactiveforms/reactiveforms.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "basics", component: BasicsComponent },
   { path: "forms", component: FormsComponent },
+  { path: "rforms", component: ReactiveformsComponent },
   { path: "**", component: PagenotfoundComponent }
 ];
 
